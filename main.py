b8063cb4917ade16bd74ffe1e03049e4ba5422e4
Marko Mušica <markomusica@gmail.com>
2022-11-10 15:50:10 +0100

    Added minuses to attendance and made broj_izostanka accurate

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from openpyxl import Workbook, load_workbook
 from student import Student
-from openpyxl.utils import get_column_letter
+from openpyxl.utils import get_column_letter,get_column_interval, column_index_from_string
 
 wb = load_workbook('documents/košarka_evidencija.xlsx')
 ws = wb.active
 #print(ws['D3'].value)
-#wb.save('documents/NovaEvidencija.xlsx')
+
 
 col_range = ws['A:U']
 row_range = ws[3:31]
 
 studenti = []
-for row in ws.iter_rows(3,35,None,None,True):
+row_number = 3
+
+for row in ws.iter_rows(3, 35, column_index_from_string('A'), column_index_from_string('H'), True):
+
+    col_number = column_index_from_string('A')
     for cell in row:
-        if cell != None and cell != "+":
+        if cell != None and cell != "+" and cell!='-':
             osoba = Student(cell)
             studenti.append(osoba)
 
@@ -21,10 +25,17 @@ for row in ws.iter_rows(3,35,None,None,True):
             osoba.broj_dolazaka += 1
 
         if cell == None:
+            ws.cell(row_number, col_number).value = "-"
+
+        if cell == '-':
             osoba.broj_izostanaka += 1
 
+        col_number += 1
+
+    row_number += 1
+
 for student in studenti:
-    print(student.ime, "- Broj dolazaka:", student.broj_dolazaka,"- Ponasanje:",student.ponasanje())
+    print(student.ime, "- Broj dolazaka:", student.broj_dolazaka,"- Ponasanje:", student.broj_izostanaka)
 
-
+wb.save('documents/košarka_evidencija.xlsx')
 
